698e1aa07fba8e01832f35e2d04f4c4f839bb2bc
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-24 09:27:32 +1300

    feat: expiration event

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,6 +1,9 @@
 export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
+
   OrderCreated = 'order:cancelled',
   OrderCancelled = 'order:cancelled',
+
+  ExpirationComplete = 'expiration:complete',
 }
